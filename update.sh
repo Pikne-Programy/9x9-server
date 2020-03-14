3bd6c799c119355041813e9cfb31dbdf2ad669b4
Nircek <nircek-2103@protonmail.com>
2020-03-14 10:38:38 +0100

    Handle forced pushes

--- update.sh
+++ update.sh
@@ -3,6 +3,6 @@ git fetch
 [[ $(git rev-parse HEAD) == $(git rev-parse @{u}) ]] && { echo "Already up to date, exitting..."; exit 0; }
 sudo systemctl stop server9x9
 sudo systemctl status server9x9
-git pull
+git pull -f || { git reset "origin/$(git rev-parse --abbrev-ref HEAD)" --hard; git pull; }
 sudo systemctl start server9x9
 sudo systemctl status server9x9
