2af9588e533cb4d1bc964f80de0ecee733d29690
Nircek <nircek-2103@protonmail.com>
2020-03-19 20:39:20 +0100

    Add handling a config file Closes #4

--- 9x9-server/__main__.py
+++ 9x9-server/__main__.py
@@ -1,5 +1,16 @@
+import os
+import configparser
+import pathlib
+
 from .server import Server
 
-PORT = 4780
+config_path = pathlib.Path(__file__).parent.parent
+def get_config_path(name):
+    return config_path.joinpath(name).absolute()
+config = configparser.ConfigParser()
+config.read([get_config_path(name) for name in ('9x9-server-default.conf', '9x9-server.conf')])
+
+PORT = config['9x9-server'].getint('PORT')
+print(f'Starting 9x9-server at {PORT}...')
 srv = Server(PORT)
 srv.start()
