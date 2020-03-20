226a20e36a7773b365f893caf50807179f4f3392
Marwyk2003 <35006711+Marwyk2003@users.noreply.github.com>
2020-03-20 20:41:48 +0100

    Revert "Adding basic game mechanics"

--- 9x9-server/game.py
+++ 9x9-server/game.py
@@ -1,27 +1,20 @@
 from threading import Lock
-from .room import Room
-
 
 import random
-
-
 def sendRandomState(c):
     # TODO: it's temporary of course and it is there only for testing
     c.send({
-        "board": ''.join([random.choice(['X', 'O']+['-']*5) for i in range(9*9)]),
-        "bigBoard": ''.join([random.choice(['X', 'O']+['-']*8) for i in range(9)]),
-        "whoWon": random.choice(['X', 'O']+['-']*30),
-        "you": random.choice(['X', 'O']),
-        "move": random.choice(['X', 'O']),
-        "marked": random.randint(-1, 8)
+        "board": ''.join([random.choice(['X','O']+['-']*5) for i in range(9*9)]),
+        "bigBoard": ''.join([random.choice(['X','O']+['-']*8) for i in range(9)]),
+        "whoWon": random.choice(['X','O']+['-']*30),
+        "you": random.choice(['X','O']),
+        "move": random.choice(['X','O']),
+        "marked": random.randint(-1,8)
     }, 'STT')
-
 
 class Game:
     def __init__(self):
         self.clients = []
-        self.rooms = []
-        self.emptyRooms = []
         self.lock = Lock()
 
     def __del__(self):
@@ -47,23 +40,7 @@ class Game:
             c.kill()
 
     def join(self, client, room):
-        if len(self.emptyRooms) == 0:
-            r = Room(self, len(self.rooms))
-            self.rooms.append(r)
-            self.emptyRooms.append(r)
-        self.emptyRooms[0].Connect(client)
-        client.roomId = self.emptyRooms[0].id
-
-        if len(self.emptyRooms[0].clients) == 2:
-            self.emptyRooms.pop(0)
+        sendRandomState(client)
 
     def set(self, client, x, y):
-        print(client.roomId)
-        if client.roomId == -1:
-            client.send({
-                "message": "You're not conected to any room!"
-            }, "BAD")
-            return
-        room = self.rooms[client.roomId]
-
-        room.Move(client, x, y)
+        sendRandomState(client)
